2a6d01de8f27bcb78a365507ab9f6f60551fef09
Astrrrrid <46434505+Astrrrrid@users.noreply.github.com>
2021-08-12 08:35:04 -0400

    Add files via upload

--- log_viewer.tpl.php
+++ log_viewer.tpl.php
@@ -7,37 +7,15 @@ $log_list = array(
 	'purge'	=> __( 'Purge Log', 'litespeed-cache' ),
 );
 
-
-
-if ( isset( $_GET['log_type'] ) ) {
-	$log_type = $_GET['log_type'];
+if ( isset( $_GET[ 'log_type' ] ) ) {
+	$log_type = $_GET[ 'log_type' ];
 	setcookie( 'litespeed_log_view' , $log_type );
-} else if ( isset( $_COOKIE['litespeed_log_view'] ) ) {
-	$log_type = $_COOKIE['litespeed_log_view'];
+} else if ( isset( $_COOKIE[ 'litespeed_log_view' ] ) ) {
+	$log_type = $_COOKIE[ 'litespeed_log_view' ];
 } else {
 	$log_type = 'debug';
 }
-
-if ( $log_type === 'purge' ) {
-	$file = LSCWP_CONTENT_DIR . '/debug.purge.log';
-} else if ( $log_type === 'crawler' ) {
-	$file = LSCWP_CONTENT_DIR . '/crawler.log';
-} else {
-	$file = LSCWP_CONTENT_DIR . '/debug.log';
-}
 ?>
-
-
-	<div class='nav-bar'>
-		<?php
-			foreach ( $log_list as $tab => $val ) {
-				$str = admin_url( 'admin.php?page=litespeed-toolbox&log_type='.$tab.'#log_viewer');
-				echo "<a href=\"" . $str . "\" class='tablinks' data-litespeed_log_tab='$tab'>$val</a>";
-			}
-		?>
-	</div>
-
-
 
 <h3 class="litespeed-title">
 	<?php echo __($log_list[$log_type], 'litespeed-cache'); ?>
@@ -46,6 +24,58 @@ if ( $log_type === 'purge' ) {
 		<?php echo __( 'Clear Log', 'litespeed-cache' ); ?>
 	</a>
 </h3>
+<div class='nav-bar'>
+	<?php
+		foreach ( $log_list as $tab => $val ) {
+			$str = admin_url( 'admin.php?page=litespeed-toolbox&log_type='.$tab.'#log_viewer');
+			echo "<a href=\"" . $str . "\" class='tablinks' data-litespeed_log_tab='$tab'>$val</a>";
+		}
+	?>
+	<div id='js-pointer' class='pointer'></div>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js"></script>
+<script>
+	const pointer = document.querySelector( '.pointer' );
+	var links = document.getElementsByClassName( 'tablinks' );
+	var tab_widths = [];
+	for( var i=0; i<links.length; i++ ){
+		var current = links[i];
+		tab_widths[i] = current.offsetWidth + 37;
+		// var ele_width = current.offsetWidth;
+		current.dataset.order = i * 110 + "%";
+		current.addEventListener( "mouseover" , movePointer );
+		pointer.style.width = tab_widths[i] + 'px';
+	}
+
+	function movePointer( e ) {
+		var order = e.currentTarget.dataset.order;
+		pointer.style.transform = "translate3d(" + order + ",0,0)";
+	}
+
+	// document.getElementsByClassName( 'nav-bar' ).addEventListener( 'mouseout', function( e ) {
+	// 	pointer.style.transform = "translate3d(" + order + ",0,0)";
+	// });
+
+
+
+	jQuery( document ).ready( function () {
+		$( "[ data-litespeed-layout = 'log_viewer' ]" ).css( 'position', 'relative' );
+		<?php
+			echo "$( \"[ data-litespeed_log_tab = '" . $log_type . "' ]\" ).css('color', '#1B9292');";
+			if ( $log_type === 'purge' ) {
+				echo "pointer.style.transform = \"translate3d(\" + links[2].dataset.order + \",0,0)\";";
+				$file = LSCWP_CONTENT_DIR . '/debug.purge.log';
+			} else if ( $log_type === 'crawler' ) {
+				echo "pointer.style.transform = \"translate3d(\" + links[1].dataset.order + \",0,0)\";";
+				$file = LSCWP_CONTENT_DIR . '/crawler.log';
+			} else {
+				// echo "$( \"[ data-litespeed_log_tab = 'debug' ]\" ).css('color', '#1b9292');";
+				$file = LSCWP_CONTENT_DIR . '/debug.log';
+			}
+		?>
+	} ); // end jquery wraper
+</script>
 <?php
 	$lines = File::count_lines( $file );
 	$start = $lines > 1000 ? $lines - 1000 : 0;
@@ -57,116 +87,77 @@ if ( $log_type === 'purge' ) {
 <a href="<?php echo Utility::build_url( Router::ACTION_DEBUG2, Debug2::TYPE_CLEAR_LOG ); ?>" class="button button-primary">
 	<?php echo __( 'Clear Log', 'litespeed-cache' ); ?>
 </a>
-
-
-
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js "></script>
-<script>
-
-
-// function showLog(evt, type) {
-//   var i, tabcontent, tablinks;
-//   tabcontent = document.getElementsByClassName("tabcontent");
-//   for (i = 0; i < tabcontent.length; i++) {
-//     tabcontent[i].style.display = "none";
-//   }
-//   tablinks = document.getElementsByClassName("tablinks");
-//   for (i = 0; i < tablinks.length; i++) {
-//     tablinks[i].className = tablinks[i].className.replace(" active", "");
-//   }
-//   // document.querySelectorAll('[data-litespeed_log_tab="type"]').style.display = "block";
-//   document.getElementById(type).style.display = "block";
-//   evt.currentTarget.className += " active";
-// }
-
-// jQuery(document).ready( function () {
-// 	var litespeed_log_view_current = document.cookie;
-// 	let searchParam = new URLSearchParams(window.location.search);
-// 	console.log( searchParam.has('log_type') );
-// 	let param = searchParam.get('log_type');
-// 	if( searchParam.has('log_type') ) {
-// 		litespeed_log_view_current = param;
-// 	}
-// 	if( !litespeed_log_view_current || !$('[data-litespeed_log_tab="'+litespeed_log_view_current+'"]').length) {
-// 		litespeed_log_view_current = $('[data-litespeed_log_tab]').first().data('litespeed-tab1') ;
-// 	}
-// 	$('[data-litespeed_log_tab]').on( 'click', function(event) {
-// 		document.cookie = 'litespeed_log_view='+$(this).data('litespeed-tab1') ;
-// 		$(this).blur() ;
-// 	});
-// } );
-
-</script>
+<!-- <link rel="stylesheet/less" href="~/Downloads/Ferrous/wp-content/plugins/litespeed-cache/tpl/toolbox/navbar.scss" /> -->
 <style type="text/css">
-	.nav-bar {
-		font-size:  1.3em;
-		/*color: #1d2327;*/
-		font-family:  -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif;
-		color: #3c434a;
-		font-weight: 600;
-		margin: 1.5em 0px 3.5em 0;
-		line-height: inherit;
+.position {
+	position: relative;
+}
 
-		padding-right:  50px;
-		padding-left: 3px;
-		padding-top: 5px;
-	}
-	/* Style the tab */
-	.tablinks {
-	  overflow: hidden;
-	  /*border: 1px solid #ccc;*/
-	  background-color: #f1f1f1;
-	}
+.my_container {
+	display: flex;
+	justify-content: space-between;
+	flex-wrap: nowrap;
+	align-items: flex-end;
+}
 
-	/* Style the buttons that are used to open the tab content */
-	.tablinks {
-	  background-color: inherit;
+.nav-bar {
+/*	text-align: right;
+	float: right;*/
+	font-size:  1.3em;
+	font-family:  -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif;
+	color: #528ac6;
+	font-weight: 600;
+	/*margin: 3em 12em 1em 3em;*/
+	line-height: inherit;
+	padding-right:  5em;
+	padding-left: 20px;
+	/*padding-top: 5px;*/
+	white-space: nowrap;
+	position: absolute;
+	right: 0;
+	top: 0;
+}
 
-	  outline: none;
-	  cursor: pointer;
-	  transition: 0.3s;
-	}
+.tablinks {
+	overflow: hidden;
+	text-align: center;
+	background-color: inherit;
+	outline: none;
+	cursor: pointer;
+	transition: 0.3s;
+	padding: 10px;
+	margin-left: -1px;
+	position: relative;
+	left: 1px;
+}
 
-	/* Change background color of buttons on hover */
-	.tablinks:hover {
-	  /*background-color: #ddd;*/
-	}
+.tablinks:hover {
+	/*color: #36b0b0;*/
+	/*text-decoration: underline;*/
+	text-shadow: #135e96;
+}
 
-	/* Create an active/current tablink class */
-	.tablinks a.active {
-	  background-color: #36b0b0;
-	}
+.tablinks:active {
+	color: 1px 1px #36b0b0;
+}
 
-	/* Style the tab content */
-	.tabcontent {
-	  display: none;
-	  padding: 6px 12px;
-	  border: 1px solid #ccc;
-	  border-top: none;
-	}
+a {
+	/*color: #528ac6;*/
+	text-decoration: none;
+}
 
-	.litespeed-tab1 {
-	  position: sticky;
-	  left: 200px;
-	  top: 150px;
-	  margin: 25px 0;
-	  display: flex;
-      align-items: flex-start;
-	}
-	.tablinks {
-	  float: left;
-	}
-	.tablinks {
-	  /*background: #eee;*/
-	  padding: 10px;
-	  /*border: 1px solid #ccc;*/
-	  margin-left: -1px;
-	  position: relative;
-	  left: 1px;
-	}
-	a {
-		color: #3c434a;
-		text-decoration: none;
-	}
+.pointer {
+	z-index: 1;
+	margin-top: 0.3em;
+	margin-left: 0.2em;
+	top: 0.6em;
+	left: 1em;
+	background-color: #36b0b0;
+	height: 0.25em;
+	transition: transform 0.25s ease-in-out;
+	border-radius: 0.3em;
+	will-change: transform;
+	backface-visibility: hidden;
 
+}
 </style>
